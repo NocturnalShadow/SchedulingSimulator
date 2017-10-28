5b9f1bfd56a981c9c87d6af7a71fb3b0f5f9a133
NocturnalShadow <bogdanscloud@gmail.com>
2017-10-28 23:26:42 +0300

    Implemented Multi-level queue scheduling

--- src/main/java/edu/modernos/Scheduler.java
+++ src/main/java/edu/modernos/Scheduler.java
@@ -14,43 +14,82 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class Scheduler {
     private static void Log(Process process, String event, PrintStream out) {
-        out.format("%-3s %-14s %-6s %6s ms | %-6s%n",
-                process.id, event, process.ioBlockInterval + " ms", process.cpuTimeUsed, process.cpuTimeNeeded + " ms");
+        out.format("%-3s %-13s %-7s %-7s %-6s %6s ms | %-6s%n",
+                process.id, event, process.priority,
+                process.quantum + " ms",
+                process.ioBlockInterval + " ms",
+                process.cpuTimeUsed,
+                process.cpuTimeNeeded + " ms");
     }
 
     public static Result Run(LinkedList<Process> processes, int maxRunTime, String logFileName) {
-        Queue<Process> processQueue = new LinkedBlockingQueue<Process>(processes);
-        Process defaultProcess = new Process();
-        processQueue.add(defaultProcess);
+        Queue<Process> processQueue = new LinkedBlockingQueue<Process>();
+        Queue<Process> pendingProcessQueue = new LinkedBlockingQueue<>(processes);
+
+        Process guard = new Process();
+        processQueue.add(guard);
 
         Result result = new Result(
                 "Batch (Non-preemptive)",
                 "First-Come First-Served");
 
         try (PrintStream out = new PrintStream(new FileOutputStream(logFileName))) {
-            out.format("%-3s %-14s %-10s %10s%n",
-                    "#", "State", "I/O Block", "Progress");
+            out.format("%-3s %-10s %-9s %-8s %-10s %10s %n",
+                    "#", "State", "Priority", "Quantum", "I/O Block", "Progress");
 
-            for(Process process : processQueue) {
-                if(process == defaultProcess) {
-                    processQueue.add(processQueue.remove());
-                    continue;
+
+            int quantum = 1;
+            while(true) {
+                for(Process process : pendingProcessQueue) {
+                    processQueue.add(process);
+                    pendingProcessQueue.remove();
+                    process.quantum = quantum;
                 }
 
-                Log(process, "registered", out);
-                if(process.ioBlockInterval < process.cpuTimeNeeded - process.cpuTimeUsed && process.ioBlockInterval != 0) {
-                    result.totalRunTime += process.ioBlockInterval;
-                    process.cpuTimeUsed += process.ioBlockInterval;
-                    process.timesBlocked++;
-                    processQueue.add(processQueue.remove());
-                    Log(process, "I/O blocked", out);
-                } else {
-                    result.totalRunTime += process.cpuTimeNeeded - process.cpuTimeUsed;
-                    process.cpuTimeUsed = process.cpuTimeNeeded;
-                    processQueue.remove();
-                    Log(process, "completed", out);
+                for(Process process : processQueue) {
+                    if(process == guard) {
+                        processQueue.add(processQueue.remove());
+                        continue;
+                    }
+
+                    Log(process, "registered", out);
+
+                    while(true) {
+                        result.totalRunTime++;
+
+                        process.quantum--;
+                        process.ioNextBlock++;
+                        process.cpuTimeUsed++;
+
+                        if(process.cpuTimeNeeded == process.cpuTimeUsed) {
+                            Log(process, "completed", out);
+                            processQueue.remove();
+                            break;
+                        }
+                        if(process.ioNextBlock == process.ioBlockInterval) {
+                            Log(process, "I/O blocked", out);
+                            process.timesBlocked++;
+                            processQueue.add(processQueue.remove());
+                            break;
+                        }
+                        if(process.quantum == 0) {
+                            process.priority++;
+                            Log(process, "demoted", out);
+                            pendingProcessQueue.add(processQueue.remove());
+                            break;
+                        }
+                        if(result.totalRunTime == maxRunTime) {
+                            return result;
+                        }
+                    }
+                }
+                quantum *= 2;
+
+                if(pendingProcessQueue.isEmpty()) {
+                    return result;
                 }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
